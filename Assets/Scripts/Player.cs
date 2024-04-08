cdb9fbf7114a07a30ea2323892cf384ec350af16
sinatb <sinatb.dev@gmail.com>
2024-04-08 21:57:04 +0330

    feat : added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour
     private Transform SpawnPos;
     [SerializeField]
     private float ShootTimer = 0.25f;
+    [SerializeField]
+    private float PlayerSpeed = 1.0f;
     private bool _canSpawn = true;
 
     private IEnumerator shoot_delay()
@@ -23,16 +25,23 @@ public class Player : MonoBehaviour
     }
     private void Update()
     {
+        var xAxis = Input.GetAxis("Horizontal");
+        var yAxis = Input.GetAxis("Vertical");
+        var movementVector = new Vector3(xAxis, yAxis, 0).normalized;
         var mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var deltaX = mouse_pos.x - transform.position.x;
         var deltaY = mouse_pos.y - transform.position.y;
-        var angle = math.degrees(math.atan2(deltaY, deltaX));
-        transform.rotation = Quaternion.Euler(0,0,angle - 90.0f);
+        if (math.abs(deltaX) > 0.05f && math.abs(deltaY) > 0.05f)
+        {
+            var angle = math.degrees(math.atan2(deltaY, deltaX));
+            transform.rotation = Quaternion.Euler(0, 0, angle - 90.0f);
+        }
         if (Input.GetMouseButton(0) && _canSpawn)
         {
             Instantiate(bullet, SpawnPos.position, transform.rotation);
             _canSpawn = false;
             StartCoroutine(shoot_delay());
         }
+        transform.Translate(movementVector * PlayerSpeed * Time.deltaTime,Space.World);
     }
 }
